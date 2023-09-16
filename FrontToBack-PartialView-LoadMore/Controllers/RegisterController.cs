c04cc95fd9da6466c8de9a0aa781ccf27de61538
AtillaRustemli <rustemliatilla662@gmail.com>
2023-09-17 00:03:21 +0400

    feat:Register

---
+++ FrontToBack-PartialView-LoadMore/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using FrontToBack_PartialView_LoadMore.Entities;
+using FrontToBack_PartialView_LoadMore.ViewModels;
+using FrontToBack_PartialView_LoadMore.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack_PartialView_LoadMore.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View();
+            AppUser appUser = new();
+            appUser.FullName = registerVM.Fullname;
+            appUser.UserName = registerVM.Username;
+            appUser.Email = registerVM.Email;
+            IdentityResult result=await _userManager.CreateAsync(appUser, registerVM.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+                return View(registerVM);
+            }
+            return RedirectToAction("index","Home");
+        }
+    }
+}
