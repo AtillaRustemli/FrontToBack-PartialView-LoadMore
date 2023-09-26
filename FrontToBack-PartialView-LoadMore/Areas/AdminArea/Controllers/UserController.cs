f5d742fdab7cef1dfd32bf7d9218d184324e0754
AtillaRustemli <rustemliatilla662@gmail.com>
2023-09-26 23:28:56 +0400

    feat:UserUpdate

--- FrontToBack-PartialView-LoadMore/Areas/AdminArea/Controllers/UserController.cs
+++ FrontToBack-PartialView-LoadMore/Areas/AdminArea/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using FrontToBack_PartialView_LoadMore.DAL;
 using FrontToBack_PartialView_LoadMore.Entities;
+using FrontToBack_PartialView_LoadMore.ViewModels.Account;
+using FrontToBack_PartialView_LoadMore.ViewModels.AdminUser;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +10,15 @@ namespace FrontToBack_PartialView_LoadMore.Areas.AdminArea.Controllers
     [Area("AdminArea")]
     public class UserController : Controller
     {
+        private readonly AppDbContext _appDbContext;
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public UserController(UserManager<AppUser> userManager)
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext appDbContext)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _appDbContext = appDbContext;
         }
 
 
@@ -21,6 +27,7 @@ namespace FrontToBack_PartialView_LoadMore.Areas.AdminArea.Controllers
             var users=search==null?_userManager.Users.ToList():_userManager.Users.Where(u=>u.FullName.ToLower().Contains(search)).ToList();
             return View(users);
         }
+        #region Delete
         public async Task<IActionResult> Delete(string id)
         {
             var user=await _userManager.FindByIdAsync(id);
@@ -29,5 +36,34 @@ namespace FrontToBack_PartialView_LoadMore.Areas.AdminArea.Controllers
             return RedirectToAction("index");
 
         }
+        #endregion
+        #region Update
+        public async Task<ActionResult> Update(string id)
+        {
+            var user= await _userManager.FindByIdAsync(id);
+            var updateUserVM = new UpdateUserVM();
+            updateUserVM.Email = user.Email;
+            updateUserVM.Fullname = user.FullName;
+            updateUserVM.Username = user.UserName;
+            //
+            return View(updateUserVM);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Update(string id,UpdateUserVM updateUserVM)
+        {
+            AppUser updatedUser = await _userManager.FindByIdAsync(id);
+            if (!ModelState.IsValid) return View();
+            updatedUser.FullName = updateUserVM.Fullname;
+            updatedUser.UserName = updateUserVM.Username;
+            updatedUser.Email = updateUserVM.Email;
+            _appDbContext.SaveChanges();
+
+
+          //var result = await _signInManager.PasswordSignInAsync(user, updateUserVM.Password, updateUserVM.RememberMe, true);
+            return RedirectToAction("index");
+        }
+        #endregion
+
     }
 }
---
+++ FrontToBack-PartialView-LoadMore/ViewModels/AdminUser/UpdateUserVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontToBack_PartialView_LoadMore.ViewModels.AdminUser
+{
+    public class UpdateUserVM
+    {
+        [Required, StringLength(100)]
+        public string Fullname { get; set; }
+        [Required, StringLength(100)]
+        public string Username { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, DataType(DataType.Password), Compare(nameof(Password))]
+        public string RepeatPassword { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
