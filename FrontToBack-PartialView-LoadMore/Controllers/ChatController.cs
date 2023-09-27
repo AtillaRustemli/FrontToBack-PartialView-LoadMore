a0e46e09c9314a582cf5fda63f569b9e52d56546
AtillaRustemli <rustemliatilla662@gmail.com>
2023-09-27 22:54:25 +0400

    feat:Chat-ChatBox

--- FrontToBack-PartialView-LoadMore/Controllers/ChatController.cs
+++ FrontToBack-PartialView-LoadMore/Controllers/ChatController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FrontToBack_PartialView_LoadMore.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FrontToBack_PartialView_LoadMore.Controllers
 {
     public class ChatController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ChatController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult ChatBox()
         {
+            ViewBag.Users =_userManager.Users.ToList();
             return View();
         }
     }
